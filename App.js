d37ead17f85682e0b096324318931d4fc75e713a
ChamikaRohan <chamika707@gmail.com>
2024-05-06 21:41:48 +0530

    Created token cache for Sign-in

--- App.js
+++ App.js
@@ -2,10 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './App/Screens/LoginScreen/Login';
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
 
 export default function App() {
   return (
-    <ClerkProvider publishableKey='pk_test_a2V5LWxhbWItNzcuY2xlcmsuYWNjb3VudHMuZGV2JA'>
+    <ClerkProvider tokenCache={tokenCache} publishableKey='pk_test_a2V5LWxhbWItNzcuY2xlcmsuYWNjb3VudHMuZGV2JA'>
     <View style={styles.container}>
       {/* Sign In component */}
       <SignedIn>
