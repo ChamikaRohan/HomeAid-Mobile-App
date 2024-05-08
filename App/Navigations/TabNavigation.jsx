46675e2fb4825e2cbc3e10c7a4a3241735111dfc
ChamikaRohan <chamika707@gmail.com>
2024-05-08 21:39:21 +0530

    Updated Tab-Navigations appearance

--- App/Navigations/TabNavigation.jsx
+++ App/Navigations/TabNavigation.jsx
@@ -13,7 +13,13 @@ const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
     return (
-        <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Navigator 
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: Colors.PRIMARY
+        }}
+        >
+          
         <Tab.Screen name="home" component={HomeScreen} options={
           {tabBarLabel: ({color})=>(<Text style={{color:color, fontSize: 12, marginTop: -7 }} >Home</Text>),
           tabBarIcon: ({color, size})=>(<FontAwesome5 name="home" size={size} color={color} />)
